53fe04ef1f341fbb3bdb056df4c110c1b44b9e34
MohamedGalal <abogalalmedo@gmail.com>
2017-02-17 10:02:03 +0200

    make script Shooter
    FPS when  As use script first determines whether the mouse pointer and draw a form of beatings fire place
    As use Scripting deliberation shoot fire all to press the button Mot

---
+++ CrossHair.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour {
+    public Texture2D crossHair;
+    void OnGUI () {
+        Rect rec = new Rect((Input.mousePosition.x - crossHair.width / 4), ((Screen.height - Input.mousePosition.y+22) - crossHair.height / 4), crossHair.width / 2, crossHair.height / 2);
+        GUI.DrawTexture(rec, crossHair);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Detection.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+    private RaycastHit hit;
+    public LayerMask detectionLayer;
+    private float checkRate = 0.5f;
+    private float nextCheck;
+    // private float ring = 5;
+    private Transform myTransform;
+
+    public GameObject killEfect;
+
+    public GameObject enemy;
+   /// public ParticleSystem hitEffect;
+   /// 
+
+   // DestroyEnemy de;
+   
+    void Start()
+    {
+       // hitEffect.enableEmission = false;
+        IntialReferance();
+
+       // de = new DestroyEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectionItem();
+    }
+
+    void IntialReferance()
+    {
+
+        myTransform = transform;
+        // detectionLayer = 1 << 9;
+
+    }
+
+
+    void RandomPostion()
+    {
+
+        float x = Random.Range(-5.0f, 50f);
+        float z = Random.Range(-5.0f, 50f);
+        enemy.transform.position=new Vector3(x, 0, z);
+    }
+
+    void DetectionItem()
+    {
+        if (Input.GetButtonDown("Fire1")) { 
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            if (Physics.Raycast(myTransform.position, -myTransform.forward, out hit, Mathf.Infinity, detectionLayer))
+            {
+                Debug.Log(hit.transform.name + "is Layer");
+                Debug.DrawLine(transform.position, hit.point, Color.cyan);
+
+
+               // hitEffect.transform.position = hit.point;
+               // hitEffect.enableEmission = true;
+                Instantiate(killEfect, hit.transform.position, hit.transform.rotation);
+              
+                RandomPostion();
+               // de.Destroy_Enemy();
+            }
+
+
+        }
+        }
+    }
+}
